f7e00b82d032bb24afd6e4a15aaa4dd1a9b7341b
Yuliya Chernook <yuliya.chernook@gmail.com>
2021-09-04 09:54:21 +0300

    Initial commit

---
+++ app/src/main/java/chernook/fit/bstu/by/MainActivity.java
@@ -0,0 +1,14 @@
+package chernook.fit.bstu.by;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //TODO add a new method
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
