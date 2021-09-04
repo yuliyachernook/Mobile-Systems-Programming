9f26718b6f88b6632f282d5de49190a51719e812
Yuliya Chernook <yuliya.chernook@gmail.com>
2021-09-04 10:21:31 +0300

    lab1

--- app/src/main/java/chernook/fit/bstu/by/MainActivity.java
+++ app/src/main/java/chernook/fit/bstu/by/MainActivity.java
@@ -3,6 +3,10 @@ package chernook.fit.bstu.by;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import chernook.fit.bstu.by.text.TextFunction;
 
 public class MainActivity extends AppCompatActivity {
     //TODO add a new method
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int count = 0; count < 10; count++) {
+            Log.d("MainActivity", " counter =" + count);
+        }
+        displayText();
+
+    }
+
+    private void displayText() {
+        TextFunction tf = new TextFunction();
+        TextView nt = findViewById(R.id.newtest);
+        nt.setText(tf.getValue());
     }
 }
---
+++ app/src/main/java/chernook/fit/bstu/by/text/TextFunction.java
@@ -0,0 +1,7 @@
+package chernook.fit.bstu.by.text;
+
+public class TextFunction {
+    public String getValue(){
+        return "Hello from first project";
+    }
+}
